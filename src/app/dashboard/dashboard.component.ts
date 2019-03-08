7aaec54dc65d310b48a60be310bc4b3e6053dfd9
arkadiusz.sekula <arkadiusz.sekula@comarch.pl>
2019-03-08 14:26:53 +0100

    - Routing / Add a dashboard view: routing to dashboard by default

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Hero} from '../hero';
+import {HeroService} from '../hero.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  heroes: Hero[] = [];
 
-  constructor() { }
+  constructor(private heroService: HeroService) {
+  }
+
+  getHeroes(): void {
+    this.heroService
+      .getHeroes()
+      .subscribe(heroeS => this.heroes = heroeS.slice(1, 5));
+  }
 
   ngOnInit() {
+    this.getHeroes();
   }
 
 }
