d13b74cc2ffbd59437875eb506c4c5cdca696906
arqarq <43341071+arqarq@users.noreply.github.com>
2019-04-09 00:19:35 +0200

    - crisis-center module

--- src/app/module/crisis/crisis-routing.module.ts
+++ src/app/module/crisis/crisis-routing.module.ts
@@ -1,11 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CrisisCenterComponent } from './crisis-center.component';
 import { RouterModule, Routes } from '@angular/router';
+import { CrisisListComponent } from './crisis-list/crisis-list.component';
+import { HeroListComponent } from './hero-list/hero-list.component';
 
 const ROUTES: Routes = [
   {
     path: '',
-    component: CrisisCenterComponent
+    component: CrisisCenterComponent,
+    pathMatch: 'prefix',
+    children: [
+      {
+        path: 'crisis-center',
+        component: CrisisListComponent
+      },
+      {
+        path: 'heroes',
+        component: HeroListComponent
+      }
+    ]
   }
 ];
 
--- src/app/module/crisis/crisis.module.ts
+++ src/app/module/crisis/crisis.module.ts
@@ -2,9 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CrisisCenterComponent } from './crisis-center.component';
 import { CrisisRoutingModule } from './crisis-routing.module';
+import { CrisisListComponent } from './crisis-list/crisis-list.component';
+import { HeroListComponent } from './hero-list/hero-list.component';
 
 @NgModule({
-  declarations: [CrisisCenterComponent],
+  declarations: [
+    CrisisCenterComponent,
+    CrisisListComponent,
+    HeroListComponent
+  ],
   imports: [
     CommonModule,
     CrisisRoutingModule
