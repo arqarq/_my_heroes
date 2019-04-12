410582c51279bc7f4e5b4b5f16ba8922c4ed6167
Arkadiusz Sekuła <arkadiusz.sekula@comarch.pl>
2019-04-12 15:56:54 +0200

    - HEROES mock used in in-memory-data-service

--- src/app/service/in-memory-data.service.ts
+++ src/app/service/in-memory-data.service.ts
@@ -1,6 +1,7 @@
-import {Injectable} from '@angular/core';
-import {Hero} from '../model/hero';
-import {InMemoryDbService} from 'angular-in-memory-web-api';
+import { Injectable } from '@angular/core';
+import { Hero } from '../model/hero';
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+import { HEROES } from '../repository/mock-heroes';
 
 @Injectable({
   providedIn: 'root'
@@ -10,22 +11,10 @@ export class InMemoryDataService implements InMemoryDbService {
   }
 
   createDb() {
-    const HEROES = [
-      {id: 11, name: 'Mr. Nice'},
-      {id: 12, name: 'Narco'},
-      {id: 13, name: 'Bombasto'},
-      {id: 14, name: 'Celeritas'},
-      {id: 15, name: 'Magneta'},
-      {id: 16, name: 'RubberMan'},
-      {id: 17, name: 'Dynama'},
-      {id: 18, name: 'Dr IQ'},
-      {id: 19, name: 'Magma'},
-      {id: 20, name: 'Tornado'}
-    ];
     return {HEROES};
   }
 
-  genId(HEROES: Hero[]): number {
-    return HEROES.length > 0 ? Math.max(...HEROES.map(heroO => heroO.id)) + 1 : 11;
+  genId(heroes: Hero[]): number {
+    return heroes.length > 0 ? Math.max(...heroes.map(oneHero => oneHero.id)) + 1 : 11;
   }
 }
