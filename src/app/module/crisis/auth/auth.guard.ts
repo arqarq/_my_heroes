473a20398bf64838ce933594645a2dde9d0dabc3
arqarq <43341071+arqarq@users.noreply.github.com>
2019-04-20 01:28:52 +0200

    - AuthService

--- src/app/module/crisis/auth/auth.guard.ts
+++ src/app/module/crisis/auth/auth.guard.ts
@@ -1,15 +1,38 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot } from '@angular/router';
+import { ActivatedRoute, ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {
+  }
+
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): boolean {
     console.log('AuthGuard#canActivate called');
-    return true;
+    const url: string = state.url;
+    return this.checkLogin(url);
+  }
+
+  checkLogin(url: string): boolean {
+    if (this.authService.isLoggedIn) {
+      return true;
+    }
+
+    // Store the attempted URL for redirecting
+    this.authService.redirectUrl = url;
+    // Navigate to the login page with extras
+    this.router.navigate(
+      ['/login'],
+      {relativeTo: this.route});
+    return false;
   }
 }
