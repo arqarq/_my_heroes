406083ba77cbead1480e454b96b4068f985acd4f
Arkadiusz Sekuła <arkadiusz.sekula@comarch.pl>
2019-05-28 09:55:55 +0200

    * objects tested

--- src/app/gen_modules/messages/messages.component.ts
+++ src/app/gen_modules/messages/messages.component.ts
@@ -7,9 +7,14 @@ import { MessageService } from '../../service/message.service';
   styleUrls: ['./messages.component.css']
 })
 export class MessagesComponent {
+  var: {[key: string]: number | string} = {a: 7, 5: '8', 7: '9'};
+  var2: {[key: string]: number | string} = {d: 11, b: 12};
+  var3: {[key: string]: number | string} = {...this.var2, ...this.var, 6: 10};
+
   constructor(
     public messageService: MessageService
     // private messageService: MessageService // nie dla --aot
   ) {
+    console.log('=-=-=-=-=-= ', this.var3);
   }
 }
