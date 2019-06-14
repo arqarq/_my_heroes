a1332b535583088153cca47407c21043cc42c219
Arkadiusz Sekuła <arkadiusz.sekula@comarch.pl>
2019-06-14 15:48:12 +0200

    * report missing translations options in 'main.ts' (for JIT) and 'angular.json' (for AOT)

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, MissingTranslationStrategy } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { StartModule } from './app/start.module';
@@ -8,5 +8,9 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(StartModule)
+platformBrowserDynamic().bootstrapModule(
+  StartModule,
+  {
+    missingTranslation: MissingTranslationStrategy.Warning // for JIT, for AOT in 'angular.json'
+  })
   .catch(err => console.error(err));
