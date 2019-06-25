9a615bd91865e3f7d297bfa3c938c2d0faa3863c
arqarq <43341071+arqarq@users.noreply.github.com>
2019-06-25 22:48:57 +0200

    * ...

--- src/app/component/chooser/chooser.component.ts
+++ src/app/component/chooser/chooser.component.ts
@@ -3,7 +3,6 @@ import { Title } from '@angular/platform-browser';
 import { LOCALE_ID_NUMBERS } from '../../../locale/LIDs';
 import { LocalStorageService } from '../../service/local-storage.service';
 
-// const LANG_FIRST_STORE_KEY = 'lang_first';
 const LANG_INIT_STORAGE_KEY = 'lang_init';
 const LANG_STORAGE_KEY = 'lang';
 
@@ -29,7 +28,7 @@ export class ChooserComponent implements OnInit, AfterViewInit {
   ) {
     this.browserLocaleID = navigator.language.slice(0, 2);
     this.langStoredCode = this.storage.getStringStoredAtGivenKey(LANG_STORAGE_KEY);
-    this.langStored = (this.langStoredCode === this.localeId);
+    this.langStored = !!this.langStoredCode;
   }
 
   ngOnInit(): void {
@@ -37,10 +36,11 @@ export class ChooserComponent implements OnInit, AfterViewInit {
   }
 
   ngAfterViewInit() { // only once, ngAfterViewChecked() can be more than once
-    if (this.langStored) {
-      if (this.localeId !== this.langStoredCode) {
-        document.getElementById(this.langStoredCode).click();
-      }
+    if (
+      this.langStored &&
+      this.langStoredCode !== this.localeId
+    ) {
+      document.getElementById(this.langStoredCode).click();
     } else {
       if (
         this.localeId !== this.browserLocaleID &&
@@ -51,32 +51,7 @@ export class ChooserComponent implements OnInit, AfterViewInit {
         this.storage.storeStringAtGivenKey(LANG_INIT_STORAGE_KEY);
         document.getElementById(this.browserLocaleID).click();
       }
-      // else {
-      //   this.storage.storeStringAtGivenKey(LANG_INIT_STORAGE_KEY, this.localeId);
-      // }
     }
-    // const langStored = this.storage.getStringStoredAtGivenKey(LANG_STORAGE_KEY);
-    // const langInitialized = this.storage.getStringStoredAtGivenKey(LANG_INIT_STORAGE_KEY);
-    // const langFirstStored = this.storage.getStringStoredAtGivenKey(LANG_FIRST_STORE_KEY);
-    //
-    // if (Object.values(this.localeIdNumbers).includes(this.browserLocaleID)) {
-    //   if (langInitialized) {
-    //     if (langStored && langStored !== this.localeId) {
-    //       if (!langFirstStored) {
-    //         this.storage.storeStringAtGivenKey(LANG_FIRST_STORE_KEY, 'done');
-    //         this.storage.storeStringAtGivenKey(LANG_STORAGE_KEY, this.localeId);
-    //       } else {
-    //         this.storage.storeStringAtGivenKey(LANG_FIRST_STORE_KEY);
-    //         document.getElementById(langStored).click();
-    //       }
-    //     }
-    //   } else {
-    //     this.storage.storeStringAtGivenKey(LANG_INIT_STORAGE_KEY, this.browserLocaleID);
-    //     if (this.browserLocaleID !== this.localeId) {
-    //       document.getElementById(this.browserLocaleID).click();
-    //     }
-    //   }
-    // }
   }
 
   langStorageChanged(event: boolean) {
@@ -85,7 +60,7 @@ export class ChooserComponent implements OnInit, AfterViewInit {
       this.storage.storeStringAtGivenKey(LANG_STORAGE_KEY, this.localeId);
     } else {
       this.storage.removeStorageAtGivenKey(LANG_STORAGE_KEY);
-      this.storage.removeStorageAtGivenKey(LANG_INIT_STORAGE_KEY);
+      // this.storage.removeStorageAtGivenKey(LANG_INIT_STORAGE_KEY);
     }
   }
 
