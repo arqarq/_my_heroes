ee403f2640df8012ef1295b6540e871faa17222b
Arkadiusz Sekuła <arkadiusz.sekula@comarch.pl>
2019-06-27 08:05:32 +0200

    * chooser as lazy module done

--- src/app/gen_modules/chooser/chooser-routing.module.ts
+++ src/app/gen_modules/chooser/chooser-routing.module.ts
@@ -1,19 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { ChooserComponent } from './chooser.component';
 
 @NgModule({
-  imports: [RouterModule.forChild(
-    [
-      {
-        path: 'a',
-        loadChildren: ''
-      },
-      {
-        path: 'b',
-        loadChildren: ''
-      }
-    ]
-  )],
+  imports: [RouterModule.forChild([
+    {
+      path: '',
+      pathMatch: 'full',
+      component: ChooserComponent
+    }
+  ])],
   exports: [RouterModule]
 })
 export class ChooserRoutingModule {
