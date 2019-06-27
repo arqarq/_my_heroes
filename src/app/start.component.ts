54580759526bb43bb496883d4d55eb7bdcf18009
Arkadiusz Sekuła <arkadiusz.sekula@comarch.pl>
2019-06-27 12:57:23 +0200

    * StartComponent: replacing locale part of given address - works, links to internal routes can be resolved, only site language changed

--- src/app/start.component.ts
+++ src/app/start.component.ts
@@ -13,8 +13,10 @@ import { environment } from '../environments/environment';
 export class StartComponent implements OnInit {
   readonly langStored: boolean;
   readonly localeIdNumbers = LOCALE_ID_NUMBERS;
+  readonly localeIdNumbersValues: string[];
   readonly browserLocaleID: string;
   readonly langStoredCode: string;
+  readonly addressSplit: string[];
 
   constructor(
     @Inject(LOCALE_ID) public localeId: string,
@@ -25,11 +27,15 @@ export class StartComponent implements OnInit {
     this.browserLocaleID = navigator.language.slice(0, 2);
     this.langStoredCode = this.storage.getStringStoredAtGivenKey(LANG_STORAGE_KEY);
     this.langStored = !!this.langStoredCode;
+    this.localeIdNumbersValues = Object.values(this.localeIdNumbers);
+    const currentAddress = window.location.href;
+    this.addressSplit = currentAddress.split('/');
   }
 
   ngOnInit() {
-    const strings = window.location.href.split('/');
-    const address = strings[0] + '//' + strings[2];
+    // const strings = window.location.href.split('/');
+    // this.addressSplit = window.location.href.split('/');
+    // const addressSplit = strings[0] + '//' + strings[2];
     console.log('----------------------------------------------------------------------' +
       '---------------------------------------------------------------------- PROD?', environment.production);
     if (
@@ -38,17 +44,19 @@ export class StartComponent implements OnInit {
       environment.production
     ) {
       // document.getElementById(this.langStoredCode).click();
-      window.location.href = address + `/${this.langStoredCode}`;
+      // window.location.href = addressSplit + `/${this.langStoredCode}`;
+      window.location.href = this.replaceLocaleInAddress(this.langStoredCode);
     } else {
       if (
         this.localeId !== this.browserLocaleID &&
         !this.storage.checkEntryAtGivenKey(LANG_INIT_STORAGE_KEY) &&
-        Object.values(this.localeIdNumbers).includes(this.browserLocaleID)
+        this.localeIdNumbersValues.includes(this.browserLocaleID)
       ) {
         this.storage.storeStringAtGivenKey(LANG_STORAGE_KEY, this.browserLocaleID);
         this.storage.storeStringAtGivenKey(LANG_INIT_STORAGE_KEY);
         // document.getElementById(this.browserLocaleID).click();
-        window.location.href = address + `/${this.browserLocaleID}`;
+        // window.location.href = addressSplit + `/${this.browserLocaleID}`;
+        window.location.href = this.replaceLocaleInAddress(this.browserLocaleID);
       }
     }
     // console.log('------------------------');
@@ -60,5 +68,21 @@ export class StartComponent implements OnInit {
     // console.log('------------------------');
     // window.location.href = strings[0] + '//' + strings[2] + '/en';
     // this.router.navigate(['es', 'choose']);
+  }
+
+  private replaceLocaleInAddress(localeId: string): string {
+    const indexFound = this.addressSplit.findIndex((val) => this.localeIdNumbersValues.includes(val));
+    if (indexFound !== -1) {
+      this.addressSplit[indexFound] = localeId;
+      let newAddress = '';
+      this.addressSplit.forEach((val) => newAddress += val + '/');
+      return newAddress.substr(0, newAddress.length - 1);
+    }
+    // alert(this.addressSplit.length);
+    // if (this.addressSplit.length === 3) {
+    //   return this.addressSplit[0] + '//' + this.addressSplit[2] + `/${localeId}`;
+    // } else if (this.addressSplit.length > 3) {
+    //   return this.addressSplit[0] + '//' + this.addressSplit[2] + `/${localeId}`;
+    // }
   }
 }
