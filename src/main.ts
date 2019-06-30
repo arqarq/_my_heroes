90443798d4c37b7ad636322ebd144a9578bbddbc
arqarq <43341071+arqarq@users.noreply.github.com>
2019-06-30 11:43:54 +0200

    * ...

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { enableProdMode, LOCALE_ID, MissingTranslationStrategy } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { StartModule } from './app/start.module';
 import { environment } from './environments/environment';
 
@@ -13,6 +12,6 @@ platformBrowserDynamic(
 )
   .bootstrapModule(StartModule,
     {
-      missingTranslation: MissingTranslationStrategy.Warning // for JIT, for AOT in 'angular.json'
+      missingTranslation: MissingTranslationStrategy.Warning // for JIT, for AOT in 'angular.json' only
     })
   .catch((err) => console.error(err));
