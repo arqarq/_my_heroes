bde66934b75c9e0d1734537ba8eeab082fa59d69
Arkadiusz Sekuła <arkadiusz.sekula@comarch.pl>
2019-07-05 13:03:00 +0200

    * first very own node script to managing version number in 2 files at the same time ('package.json' & 'ngsw-config.json')!

---
+++ setver.js
@@ -0,0 +1,66 @@
+'use strict';
+const fs = require('fs');
+const P = 'package.json';
+const N = 'ngsw-config.json';
+const Opcje = Object.freeze({
+  MAJOR: 'major',
+  MINOR: 'minor',
+  PATCH: 'patch'
+});
+
+const majorOpt = function () {
+  tabOfNumbers[0]++;
+  tabOfNumbers[1] = 0;
+  tabOfNumbers[2] = 0;
+  tabOfNumbers = tabOfNumbers.join('.');
+};
+
+const minorOpt = function () {
+  tabOfNumbers[1]++;
+  tabOfNumbers[2] = 0;
+  tabOfNumbers = tabOfNumbers.join('.');
+};
+
+const patchOpt = function () {
+  tabOfNumbers[2]++;
+  tabOfNumbers = tabOfNumbers.join('.');
+};
+
+process.argv.forEach((val, index, obj) => {
+  console.log(`${index}: ${val} / ${obj}`);
+});
+let param = process.argv.slice(2);
+if (param.length) {
+  param = param[0].toLowerCase();
+}
+const pkgData = fs.readFileSync(P, {encoding: 'utf8'});
+const pkgObj = JSON.parse(pkgData);
+const pkgVer = pkgObj.version;
+console.log('wersja w', P + ':', pkgVer);
+const ngswData = fs.readFileSync(N, {encoding: 'utf8'});
+const ngswObj = JSON.parse(ngswData);
+const ngswVer = ngswObj.appData.ver;
+console.log('wersja w', N + ':', ngswVer);
+console.log('wersje równe?', pkgVer === ngswVer ? 'tak' : 'nie');
+let tabOfNumbers = pkgVer.split('.');
+switch (param) {
+  case Opcje.MAJOR:
+    majorOpt();
+    console.log('zwiększono o jeden MAJOR, wyzerowano resztę:', tabOfNumbers);
+    break;
+  case Opcje.MINOR:
+    minorOpt();
+    console.log('zwiększono o jeden MINOR, wyzerowano resztę:', tabOfNumbers);
+    break;
+  case Opcje.PATCH:
+    patchOpt();
+    console.log('zwiększono o jeden PATCH:', tabOfNumbers);
+    break;
+  default:
+    patchOpt();
+    console.log('nie podano parametru, więc zwiększono o jeden PATCH:', tabOfNumbers);
+}
+pkgObj.version = tabOfNumbers;
+fs.writeFileSync(P, JSON.stringify(pkgObj, null, 2) + '\n', {encoding: 'utf8'});
+ngswObj.appData.ver = tabOfNumbers;
+fs.writeFileSync(N, JSON.stringify(ngswObj, null, 2) + '\n', {encoding: 'utf8'});
