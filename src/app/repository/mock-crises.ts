683e7b27eb4e56c9e5a29e078c7f458147b1a2ca
Arkadiusz Sekuła <arkadiusz.sekula@comarch.pl>
2019-07-09 13:37:21 +0200

    * ...

--- src/app/repository/mock-crises.ts
+++ src/app/repository/mock-crises.ts
@@ -4,5 +4,6 @@ export const CRISES: Crisis[] = [
   {id: 1, name: 'Dragon Burning Cities'},
   {id: 2, name: 'Sky Rains Great White Sharks'},
   {id: 3, name: 'Giant Asteroid Heading For Earth'},
-  {id: 4, name: 'Procrastinators Meeting Delayed Again'}
+  {id: 4, name: 'Procrastinators Meeting Delayed Again'},
+  {id: 5, name: 'Demogorgon Attacked Again'}
 ];
