8a67a815fa5663020796488973fc6e36429415b5
Arkadiusz Sekuła <arkadiusz.sekula@comarch.pl>
2019-07-24 14:25:53 +0200

    * ...

--- src/app/service/lang-change-relay.service.ts
+++ src/app/service/lang-change-relay.service.ts
@@ -16,7 +16,7 @@ export class LangChangeRelayService {
 
   resetFlag(timeout = 1000) {
     if (this.$langSwitched) {
-      setTimeout(() => this.$langSwitched = false, timeout);
+      setTimeout(() => this.langSwitched = false, timeout);
     }
   }
 }
