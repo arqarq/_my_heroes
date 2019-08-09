07ebf44c40f4b791fe7ceb2d462eda0a7c55017f
arqarq <43341071+arqarq@users.noreply.github.com>
2019-08-09 21:36:54 +0200

    * cleaning up the mess

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { enableProdMode, LOCALE_ID, MissingTranslationStrategy } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { StartModule } from './app/start.module';
 import { environment } from './environments/environment';
+import { StartModule } from './app/start.module';
 
 if (environment.production) {
   enableProdMode();
@@ -14,8 +14,6 @@ console.log('-------------------------------------------------------------------
   '---------------------------------------------------------------------- %cPROD? %c' + environment.production,
   'color: red', 'color: red; font-weight: bolder');
 
-platformBrowserDynamic(
-  [{provide: LOCALE_ID, useValue: 'pl'}]) // dla 'ng build' wystarczy tylko tu, bez registerLocaleData() w AppModule
-  .bootstrapModule(StartModule,
-    {missingTranslation: MissingTranslationStrategy.Warning}) // for JIT, for AOT in 'angular.json' only
+platformBrowserDynamic([{provide: LOCALE_ID, useValue: 'pl'}]) // dla 'ng build' wystarczy tylko tu, bez registerLocaleData() w AppModule
+  .bootstrapModule(StartModule, {missingTranslation: MissingTranslationStrategy.Warning}) // for JIT, for AOT put in 'angular.json' only
   .catch((err) => console.error(err));
