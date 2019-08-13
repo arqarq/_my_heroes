64823dcec5c37535749c40ae40c3006452ea5df7
Arkadiusz Sekuła <arkadiusz.sekula@comarch.pl>
2019-08-13 14:41:49 +0200

    * 'en-GB' added

--- src/locale/LIDs.ts
+++ src/locale/LIDs.ts
@@ -1,6 +1,7 @@
 export const LOCALE_ID_NUMBERS: {[key: number]: string} = { // keys of object as numbers - only indexer access allowed in TS
-  1033: 'en',
-  1045: 'pl'
+  1033: 'en', // 1033: 'en-US'
+  1045: 'pl',
+  2057: 'en-GB'
 };
 
 export const LOCALE_ID_CURRENCIES: {[key: string]: {code: string, display: string}} = {
