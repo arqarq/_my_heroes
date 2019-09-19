10cef14316d20917119692aa28909d727b5e0cf3
arqarq <43341071+arqarq@users.noreply.github.com>
2019-09-19 23:27:45 +0200

    * Web Worker added in ChooserComponent

--- src/app/gen_modules/chooser/chooser.component.ts
+++ src/app/gen_modules/chooser/chooser.component.ts
@@ -1,13 +1,9 @@
 import { Component, Inject, LOCALE_ID, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { LOCALE_ID_NUMBERS } from '../../../locale/LIDs';
-import {
-  LANG_INIT_STORAGE_KEY,
-  LANG_STORAGE_KEY,
-  LANG_USER_IS_SWITCHING,
-  LocalStorageService
-} from '../../service/local-storage.service';
+import { LANG_INIT_STORAGE_KEY, LANG_STORAGE_KEY, LANG_USER_IS_SWITCHING, LocalStorageService } from '../../service/local-storage.service';
 import { LangChangeRelayService } from '../../service/lang-change-relay.service';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-chooser',
@@ -33,6 +29,7 @@ export class ChooserComponent implements OnInit {
     this.browserLocaleID = navigator.language.slice(0, 2);
     this.langStoredCode = this.storage.getStringStoredAtGivenKey(LANG_STORAGE_KEY);
     this.langStored = !!this.langStoredCode;
+    this.initWorker();
   }
 
   ngOnInit(): void {
@@ -55,6 +52,25 @@ export class ChooserComponent implements OnInit {
       this.storage.storeStringAtGivenKey(LANG_USER_IS_SWITCHING);
       el.href = '/' + el.id;
       el.click();
+    }
+  }
+
+  private initWorker() {
+    if (typeof Worker !== 'undefined') {
+      // Create a new
+      const worker = new Worker('../../worator.worker', {type: 'module'});
+      worker.onmessage = ({data}) => {
+        console.log(`page got message: ${data}`);
+      };
+      worker.postMessage('"hello"');
+    } else {
+      // Web Workers are not supported in this environment.
+      // You should add a fallback so that your program still executes correctly.
+      if (environment.isNode) {
+        console.log('Web workers not allowed here!');
+      } else {
+        alert('Web workers not allowed here!');
+      }
     }
   }
 
