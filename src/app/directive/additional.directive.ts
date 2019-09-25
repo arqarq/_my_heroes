7d334220bb32893c59ba7c3e5368e50e47cac895
arqarq <43341071+arqarq@users.noreply.github.com>
2019-09-25 19:34:45 +0200

    * document injected (2 ways) in AdditionalDirective

--- src/app/directive/additional.directive.ts
+++ src/app/directive/additional.directive.ts
@@ -1,19 +1,39 @@
 import { Directive, Inject, InjectionToken } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 export const INJ_TOKEN = new InjectionToken<Window>('Window class token');
+export const INJ2_TOKEN = new InjectionToken<Document>('Document class token');
 
 export function _window(): Window {
   return window;
 }
 
+export function _document(): Document {
+  return document;
+}
+
 @Directive({
   selector: '[appAddDblClick]',
   providers: [
-    {provide: INJ_TOKEN, useFactory: _window}
+    {provide: INJ_TOKEN, useFactory: _window},
+    {provide: INJ2_TOKEN, useFactory: _document}
   ]
 })
 export class AdditionalDirective {
-  constructor(@Inject(INJ_TOKEN) private win: Window) {
-    win.addEventListener('dblclick', () => alert('Podwójny klik!'));
+  constructor(
+    @Inject(INJ_TOKEN) private win: Window,
+    @Inject(DOCUMENT) private doc: Document,
+    @Inject(INJ2_TOKEN) private docc: Document
+  ) {
+    win.addEventListener('dblclick', () => alert(`Podwójny klik! (URI: ${AdditionalDirective.prepareURI(doc.documentURI)}) ` +
+      ` (fullscreen enabled? ${docc.fullscreenEnabled})`));
+  }
+
+  private static prepareURI(input: string): string {
+    const strings = input.split('/');
+    if (strings[2].length > 25) {
+      strings[2] = strings[2].slice(0, 8) + '...' + strings[2].slice(strings[2].length - 8);
+    }
+    return strings.join('/');
   }
 }
