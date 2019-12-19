19471a021e677e1c8f956287df321163b0f561f0
Arkadiusz Sekuła <arkadiusz.sekula@comarch.pl>
2019-12-19 08:32:53 +0100

    * 'package.json': "configuration=xx" <-> "configuration xx"

--- docs/polyfills.js
+++ docs/polyfills.js
@@ -6268,8 +6268,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! E:\Users\arqarq\IdeaProjects\_my_heroes\src\polyfills.ts */"./src/polyfills.ts");
-module.exports = __webpack_require__(/*! E:\Users\arqarq\IdeaProjects\_my_heroes\node_modules\@angular-devkit\build-angular\src\angular-cli-files\models\jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
+__webpack_require__(/*! C:\Users\Comarch\IdeaProjects\_my_heroes\src\polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! C:\Users\Comarch\IdeaProjects\_my_heroes\node_modules\@angular-devkit\build-angular\src\angular-cli-files\models\jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
 
 
 /***/ })
