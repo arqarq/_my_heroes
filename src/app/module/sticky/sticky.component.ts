67ef3ba668a13e1ee5fcb02bad762aa94ac5ee8c
arqarq <43341071+arqarq@users.noreply.github.com>
2020-02-01 00:26:51 +0100

    * sticky with background

--- src/app/module/sticky/sticky.component.ts
+++ src/app/module/sticky/sticky.component.ts
@@ -1,74 +1,53 @@
-import { AfterViewChecked, Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 const TOP_STICKY = 50;
-const TOP_RELATIVE = 150;
 
 @Component({
   selector: 'app-sticky',
   templateUrl: './sticky.component.html',
   styleUrls: ['./sticky.component.css']
 })
-export class StickyComponent implements OnInit, AfterViewChecked, OnDestroy {
+export class StickyComponent implements OnInit, OnDestroy {
   private elementById: HTMLDivElement;
   private flag = false;
-  // private intervalId;
   private offset;
-  private topInit;
 
   constructor() {
   }
 
   ngOnInit(): void {
     this.elementById = document.getElementById('handle') as HTMLDivElement;
-    // this.intervalId = setInterval(() => {
-    //   if (!this.flag) {
-    //     this.setSticky();
-    //     return;
-    //   }
-    //   this.setRelative();
-    //   console.log('elementById.offsetTop', this.elementById.offsetTop,
-    //     'elementById.clientTop', this.elementById.clientTop,
-    //     'elementById.scrollTop', this.elementById.scrollTop);
-    // }, 1500);
-    document.onload = () => {
-      this.topInit = this.elementById.getBoundingClientRect().top;
-    };
+    console.log('!', this.elementById.offsetTop, 'ngOnInit:', this.elementById.getBoundingClientRect().top);
     window.onscroll = () => {
-      // console.log('window.pageYOffset:', window.pageYOffset);
       if (!this.offset) {
-        this.offset = this.elementById.offsetTop;
-        console.log('!', this.offset, this.elementById.getBoundingClientRect().top, this.topInit);
+        console.log('!!', this.offset = this.elementById.offsetTop, 'onscroll:', this.elementById.getBoundingClientRect().top);
       }
-      if (window.pageYOffset >= this.offset - TOP_STICKY) {
+      if (window.pageYOffset >= this.offset + 33 - TOP_STICKY) {
         if (!this.flag) {
           this.setSticky();
         }
-      } else {
-        if (this.flag) {
-          this.setRelative();
-        }
+        return;
+      }
+      if (this.flag) {
+        this.setRelative();
       }
     };
   }
 
-  ngAfterViewChecked() {
-  }
-
   ngOnDestroy(): void {
-    // clearInterval(this.intervalId);
     window.onscroll = null;
   }
 
   private setRelative() {
-    this.elementById.style.position = 'relative';
-    this.elementById.style.top = TOP_RELATIVE + 'px';
-    this.elementById.style.left = '1350px';
+    this.elementById.style.position = null;
+    this.elementById.style.top = null;
+    this.elementById.style.left = null;
     this.flag = false;
   }
 
   private setSticky() {
     this.elementById.style.position = 'fixed';
-    this.elementById.style.top = /*'183px'*/TOP_STICKY + 'px';
+    this.elementById.style.top = TOP_STICKY + 'px';
     this.elementById.style.left = '1383px';
     this.flag = true;
   }
