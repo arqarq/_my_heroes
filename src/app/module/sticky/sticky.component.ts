5f115bf41a4d7b653be2783e9b09c283b07c9222
arqarq <43341071+arqarq@users.noreply.github.com>
2020-02-02 12:32:32 +0100

    * blue sticky's position check against window (client) boundaries

--- src/app/module/sticky/sticky.component.ts
+++ src/app/module/sticky/sticky.component.ts
@@ -42,49 +42,66 @@ export class StickyComponent implements OnInit, OnDestroy {
 
   onMouseDown(event: MouseEvent) {
     event.preventDefault();
+    let barSizeVert = window.innerWidth - document.documentElement.offsetWidth;
+    let barSizeHor = window.innerHeight - document.documentElement.clientHeight;
     let pos3 = event.clientX;
     let pos4 = event.clientY;
     let flag;
     let flag2;
     document.onmouseup = () => {
       document.onmousemove = document.onmouseup = null;
+      const offsetLeft = this.divElement2.offsetLeft;
+      const offsetTop = this.divElement2.offsetTop;
+      const offsetWidth = this.divElement2.offsetWidth;
+      const offsetHeight = this.divElement2.offsetHeight;
+      if (offsetLeft < 0) {
+        this.divElement2.style.left = '0';
+      } else if (offsetLeft > window.innerWidth - barSizeVert - offsetWidth) {
+        this.divElement2.style.right = '0';
+        this.divElement2.style.left = 'unset';
+      }
+      if (offsetTop < 0) {
+        this.divElement2.style.top = '0';
+      } else if (offsetTop > window.innerHeight - barSizeHor - offsetHeight) {
+        this.divElement2.style.bottom = '0';
+        this.divElement2.style.top = 'unset';
+      }
     };
     document.onmousemove = (event2: MouseEvent) => {
       event2.preventDefault();
       // console.log('tick ----');
       // tslint:disable-next-line:no-conditional-assignment
-      if (flag = !flag) {
-        // tslint:disable-next-line:no-conditional-assignment
-        if (flag2 = !flag2) {
-          // console.log('tick tock');
-          this.divElement2.style.top = (this.divElement2.offsetTop - pos4 + event2.clientY) + 'px';
-          this.divElement2.style.left = (this.divElement2.offsetLeft - pos3 + event2.clientX) + 'px';
-          this.divElement2.style.bottom = null;
-          this.divElement2.style.right = null;
-          pos3 = event2.clientX;
-          pos4 = event2.clientY;
-        }
+      if ((flag = !flag) && (flag2 = !flag2)) {
+        // console.log('tick tock');
+        this.divElement2.style.top = (this.divElement2.offsetTop - pos4 + event2.clientY) + 'px';
+        this.divElement2.style.left = (this.divElement2.offsetLeft - pos3 + event2.clientX) + 'px';
+        this.divElement2.style.bottom = null;
+        this.divElement2.style.right = null;
+        pos3 = event2.clientX;
+        pos4 = event2.clientY;
       }
     };
     if (!window.onresize) {
       console.log('set!');
+      barSizeVert = window.innerWidth - document.documentElement.offsetWidth;
+      barSizeHor = window.innerHeight - document.documentElement.clientHeight;
       let flag3;
       let flag4;
       window.onresize = () => {
-        if (this.divElement2.offsetLeft + this.divElement2.offsetWidth / 3 > window.innerWidth) {
-          this.divElement2.style.left = 'unset';
+        if (this.divElement2.offsetLeft + this.divElement2.offsetWidth /*/ 3 */ > window.innerWidth - barSizeVert) {
           this.divElement2.style.right = '0';
+          this.divElement2.style.left = 'unset';
           console.log('fired! X');
           flag3 = true;
         }
-        if (this.divElement2.offsetTop + this.divElement2.offsetHeight / 3 > window.innerHeight) {
-          this.divElement2.style.top = 'unset';
+        if (this.divElement2.offsetTop + this.divElement2.offsetHeight /*/ 3 */ > window.innerHeight - barSizeHor) {
           this.divElement2.style.bottom = '0';
+          this.divElement2.style.top = 'unset';
           console.log('fired! Y');
           flag4 = true;
         }
         // tslint:disable-next-line:no-unused-expression
-        flag3 && flag4 && (window.onresize = null);
+        flag3 && flag4 && !(window.onresize = null) && console.log('unset!');
         // tslint:disable-next-line:no-unused-expression
         flag3 || flag4 || console.log('nothing! X: ok, Y: ok');
       };
