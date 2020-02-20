52d13715f82d90df5cefa88e03734cdf8039c9c9
arqarq <43341071+arqarq@users.noreply.github.com>
2020-02-20 23:18:33 +0100

    * ...

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,7 +9,8 @@ export const environment = {
   isNode: undefined,
   firebaseConf: {
     projectId: 'd00af17f5d630b7296f102d',
-    apiKey: 'AIzaSyCZo8XF6xO0QicyVNmM9J3nM9THOQZrHeQ'
+    apiKey: 'AIzaSyCZo8XF6xO0QicyVNmM9J3nM9THOQZrHeQ',
+    serviceAccountId: 'firebase-adminsdk-fmgzc@d00af17f5d630b7296f102d.iam.gserviceaccount.com'
   }
 };
 
