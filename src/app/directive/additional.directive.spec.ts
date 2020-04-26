778b1c9b68321344d5dad38c5ba1c4c12fa067b0
arqarq <43341071+arqarq@users.noreply.github.com>
2020-04-27 00:21:11 +0200

    * FormComponent instantiation test 'should create'

--- src/app/directive/additional.directive.spec.ts
+++ src/app/directive/additional.directive.spec.ts
@@ -2,7 +2,9 @@ import { AdditionalDirective } from './additional.directive';
 
 describe('AdditionalDirective', () => {
   it('should create an instance', () => {
-    const directive = new AdditionalDirective();
+    const directive = new AdditionalDirective(
+      null, null, null, null // dirty fix
+    );
     expect(directive).toBeTruthy();
   });
 });
--- src/app/module/forms-template/form.component.spec.ts
+++ src/app/module/forms-template/form.component.spec.ts
@@ -1,25 +1,51 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { FormComponent } from './form.component';
+import { Router } from '@angular/router';
+import { CloudFirebaseService } from '../../service/cloud-firebase.service';
+import { StartModule } from '../../start.module';
+import { StartComponent } from '../../start.component';
+import { Observable } from 'rxjs';
 
 describe('FormComponent', () => {
-  let component: FormComponent;
-  let fixture: ComponentFixture<FormComponent>;
+  const cloudFirebaseServiceStub = {
+    setCollectionAndDocument: () => 0,
+    checkIfLoggedIn2: () => new Observable(),
+    checkIfLoggedIn: () => true,
+    getDocumentDataAtIndex: () => new Observable()
+  }
+  const bodyRefStub = {
+    classList: {
+      add: () => void (0)
+    },
+    restoreClassesOfBody() {
+    }
+  }
+  let component: FormComponent
+  let router: Partial<Router>
+  let cFService: Partial<CloudFirebaseService>
+  let fixture: ComponentFixture<FormComponent>
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ FormComponent ]
-    })
-    .compileComponents();
-  }));
+      declarations: [FormComponent],
+      imports: [StartModule],
+      providers: [
+        {provide: Router, useValue: {}},
+        {provide: CloudFirebaseService, useValue: cloudFirebaseServiceStub}
+      ]
+    }).compileComponents();
+  }))
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(FormComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    StartComponent.bodyRef = bodyRefStub
+    router = TestBed.inject(Router)
+    cFService = TestBed.inject(CloudFirebaseService)
+    fixture = TestBed.createComponent(FormComponent)
+    component = fixture.componentInstance
+    fixture.detectChanges()
+  })
 
   it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+    expect(component).toBeTruthy()
+  })
+})
